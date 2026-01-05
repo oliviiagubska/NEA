8ee6596aa12b0d8812179fe4c0e22092b702a988
oliviiagubska <lon24gubs01@mpw.ac.uk>
2026-01-05 22:01:29 +0000

    dtbs2

--- create_tables.py
+++ create_tables.py
@@ -5,19 +5,18 @@ DB_NAME = "login.db"
 DB_PATH = os.path.join(os.path.dirname(__file__), DB_NAME)
 
 def create_tables():
-    con = sqlite3.connect(DB_NAME)
+    con = sqlite3.connect(DB_PATH)
     cur = con.cursor()
     cur.execute("PRAGMA foreign_keys = ON;")
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users (
-       username TEXT PRIMARY KEY,
-       password TEXT NOT NULL,
-       failed_attempts INTEGER NOT NULL DEFAULT 0,
-       locked_until TEXT
-   );
-   """)
-
+        username TEXT PRIMARY KEY,
+        password TEXT NOT NULL,
+        failed_attempts INTEGER NOT NULL DEFAULT 0,
+        locked_until TEXT
+    );
+    """)
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS teams (
@@ -32,19 +31,7 @@ def create_tables():
     CREATE TABLE IF NOT EXISTS team_members (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         team_id INTEGER NOT NULL,
-        email TEXT NOT NULL,
-        FOREIGN KEY (team_id) REFERENCES teams(id)
-    );
-    """)
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        team_id INTEGER NOT NULL,
-        title TEXT NOT NULL,
-        is_done INTEGER NOT NULL DEFAULT 0,
-        created_by TEXT NOT NULL,
-        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        username TEXT NOT NULL,
         FOREIGN KEY (team_id) REFERENCES teams(id)
     );
     """)
